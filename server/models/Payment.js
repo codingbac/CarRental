8f2e915f3039cec8b9e230bd7abc30dd9d58f1cc
codingbac <125853535+codingbac@users.noreply.github.com>
2026-09-17 04:46:09 +0545

    Add eSewa payment model

---
+++ server/models/Payment.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const paymentSchema = new mongoose.Schema({
+    transactionUuid: { type: String, required: true, unique: true },
+    user: { type: ObjectId, ref: "User", required: true },
+    car: { type: ObjectId, ref: "Car", required: true },
+    pickupDate: { type: Date, required: true },
+    returnDate: { type: Date, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, enum: ["PENDING", "COMPLETE", "FAILED"], default: "PENDING" },
+    transactionCode: { type: String, default: null }
+}, { timestamps: true });
+
+const Payment = mongoose.model("Payment", paymentSchema);
+
+export default Payment;
